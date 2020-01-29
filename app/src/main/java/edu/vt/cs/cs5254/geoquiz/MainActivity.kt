b186a687933d6271150bf274408e675c8417f119
Matt Hardner <matthardner@gmail.com>
2020-01-29 07:00:57 -0500

    Added land display

--- app/src/main/java/edu/vt/cs/cs5254/geoquiz/MainActivity.kt
+++ app/src/main/java/edu/vt/cs/cs5254/geoquiz/MainActivity.kt
@@ -2,11 +2,13 @@ package edu.vt.cs.cs5254.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import edu.vt.cs.cs5254.geoquiz.R
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -46,6 +48,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -69,7 +72,28 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
 
         }
-        private fun updateQuestion(){
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    private fun updateQuestion(){
             val questionTextResId = questionBank[currentIndex].textResId
             questionTextView.setText(questionTextResId)
 
